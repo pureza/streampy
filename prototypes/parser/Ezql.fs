c91d427cee62c24fbabb2e74b947702202d198f4
Luis Pureza <pureza@gmail.com>
2009-01-05 18:04:03 +0000

    Some more stuff added to the parser

--- prototypes/parser/Ezql.fs
+++ prototypes/parser/Ezql.fs
@@ -7,13 +7,21 @@ open EzqlParser
 open EzqlAst
 
 let parse lexbuf =
-    EzqlParser.start EzqlLexer.token lexbuf
+    try
+        EzqlParser.start EzqlLexer.token lexbuf
+    with e ->
+        let pos = lexbuf.EndPos
+        failwithf "Error near line %d, character %d\n" pos.Line pos.Column
+
 
 let rec printProg = function
   | Prog classes -> List.iter printClass classes
 
 and printClass = function
-  | Class (name, methods) -> printfn "class %s" name
+  | Class (name, methods) -> printfn "class %s" name;
+                             List.iter printMethods methods
+and printMethods = function
+  | Method (name, args, code) -> printfn " - %s()" name
 
 
 let file = File.OpenText(Sys.argv.[1])
