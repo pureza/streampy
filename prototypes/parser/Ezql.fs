540b6df1e8fc3a562c15ba362d230053e3a4e10e
Luis Pureza <pureza@gmail.com>
2009-01-16 16:52:51 +0000

    More stuff added to the parser. Continuing to work on the tutorial

--- prototypes/parser/Ezql.fs
+++ prototypes/parser/Ezql.fs
@@ -6,13 +6,18 @@ open EzqlLexer
 open EzqlParser
 open EzqlAst
 
-let parse lexbuf =
+let parse file =
+    let lexbuf = Lexing.from_text_reader Encoding.ASCII file
     try
         EzqlParser.start EzqlLexer.token lexbuf
     with e ->
         let pos = lexbuf.EndPos
         failwithf "Error near line %d, character %d\n" pos.Line pos.Column
 
+let printTokens file =
+    let lexbuf = Lexing.from_text_reader Encoding.ASCII file
+    while not lexbuf.IsPastEndOfStream do
+        printfn "%A" (EzqlLexer.token lexbuf)
 
 let rec printProg = function
   | Prog classes -> List.iter printClass classes
@@ -25,11 +30,10 @@ and printMethods = function
 
 
 let file = File.OpenText(Sys.argv.[1])
-let lexbuf = Lexing.from_text_reader Encoding.ASCII file
-let result = parse lexbuf
-
-
+let result = parse file
 printProg result
+
+printTokens(File.OpenText(Sys.argv.[1]))
 
 
 
--- prototypes/parser/EzqlAst.fs
+++ prototypes/parser/EzqlAst.fs
@@ -4,15 +4,33 @@ type prog = Prog of clss list
 
 and clss  = Class of string * mthd list
 
-and mthd  = Method of string * id list * expr list
+and mthd  = Method of string * id list * expr
 
-and expr  =
+and expr =
+  | BooleanExpr of op * expr * expr
   | MethodCall of expr * id * expr list
-  | Lambda of id list * expr list
+  | Call of expr * expr list
+  | New of id * expr list
+  | Binding of id * expr * expr
+  | Lambda of id list * expr
+  | IfThenElse of expr * expr * expr option
+  | Index of expr * expr
+  | Tuple of expr list
+  | Not of expr
   | Integer of int
   | Symbol of string
   | Id of id
   | Assign of string * expr
+
+and op =
+  | OpGt
+  | OpGte
+  | OpEq
+  | OpDiff
+  | OpLte
+  | OpLt
+  | OpAnd
+  | OpOr
 
 and id = Identifier of string
 
