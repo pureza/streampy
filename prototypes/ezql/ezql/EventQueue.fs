b071573870db1ac5779a22f990d5af58279025a9
unknown <pureza@.(none)>
2009-02-11 16:59:21 +0000

    We got windows

---
+++ prototypes/ezql/ezql/EventQueue.fs
@@ -0,0 +1,22 @@
+﻿#light
+
+module EventQueue
+
+    open System
+    open System.Collections.Generic
+    open System.Timers
+
+    type EventQueue() =
+        let queue = SortedList<DateTime, (unit -> unit)>()
+        let timer = new Timer(Enabled = true, AutoReset = false)
+        let ReSchedule() = if queue.Count <> 0
+                               then timer.Interval <- float (queue.Keys.[0] - DateTime.Now).TotalMilliseconds
+        
+        do timer.Elapsed.Add(fun _ -> queue.Values.[0] ()
+                                      queue.RemoveAt 0
+                                      ReSchedule ())
+                                      
+        member self.Register(time, callback) = queue.Add (time, callback)
+                                               ReSchedule ()
+
+    let instance = EventQueue ()
---
+++ prototypes/ezql/ezql/Types.fs
@@ -0,0 +1,48 @@
+﻿#light
+
+open System
+open EzqlAst
+open System.Collections.Generic
+open Stream
+open Window
+open System.Text
+
+type context = (string * value) list
+
+and value =
+    | Integer of int
+    | Boolean of bool
+    | Sym of symbol
+    | Time of int * timeUnit
+    | Stream of Stream.Stream<IEvent>
+    | Window of Window<IEvent>
+    | Closure of context * expr
+    | Event of IEvent
+    
+and IEvent =
+    abstract Timestamp : DateTime
+        with get, set
+    abstract Item : string -> value
+        with get, set
+        
+and Event() =
+    let fields = Dictionary<string, value>()
+    let mutable timestamp = DateTime.Now
+    interface IEvent with
+        member self.Timestamp
+            with get() = timestamp and
+                 set v = timestamp <- v
+        member self.Item 
+            with get field = fields.[field] and 
+                 set field v = fields.[field] <- v   
+    override self.ToString() =
+        let mutable buffer = StringBuilder("{ @ ")
+        buffer <- buffer.Append(timestamp.TimeOfDay)
+        for key in fields.Keys do
+            buffer <- buffer.Append(sprintf " %s: %A " key fields.[key])
+        buffer.Append("}").ToString()
+
+let toSeconds value unit =
+    match unit with
+    | Sec -> value
+    | Min -> value * 60          
